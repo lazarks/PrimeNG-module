be722c8e66792e9848da31185c6e5ff580d62625
Lazar <sljivic.lazar037@gmail.com>
2022-05-31 10:47:33 +0200

    app module

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimeNGModule } from './prime-ng.module';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        PrimeNGModule,
+      ],
+      exports:[
+        BrowserModule,
+        BrowserAnimationsModule
+        PrimeNGModule,
+      ],
+      providers: [],
+      bootstrap: [AppComponent]
+})
+export class AppModule { }
